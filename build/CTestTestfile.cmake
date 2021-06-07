8a5a6b6f67bc091f49c48fb5de88c291c486472e
Adriano <38436035+Rahjael@users.noreply.github.com>
2021-06-07 18:31:07 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rahjael/Coding/INIEditor_test
-# Build directory: /home/rahjael/Coding/INIEditor_test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googletest")
-subdirs("test")
